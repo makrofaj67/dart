a2dccbebb6d072869410b5b05c5596b81d492676
makrofaj67 <114802232+makrofaj67@users.noreply.github.com>
2023-04-30 11:54:10 +0300

    Update madlibs.dart

--- bir/madlibs.dart
+++ bir/madlibs.dart
@@ -5,6 +5,8 @@ void main() {
   String ozne = stdin.readLineSync()!;
   print("zarf girin");
   String zarf = stdin.readLineSync()!;
+  print("yüklem girin");
+  String yuklem = stdin.readLineSync()!;
 
   print("$ozne çağları $zarf bir şekilde sikti");
 }
